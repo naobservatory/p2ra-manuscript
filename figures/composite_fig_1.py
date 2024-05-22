539499018ec8d64628d319f8f215ecd49fc05c53
simonleandergrimm <simonleandergrimm@gmail.com>
2024-05-22 14:55:23 -0400

    Saving interim progress. Code goes until shape_vir_comp_df and shape_hv_genus_df.

--- figures/composite_fig_1.py
+++ figures/composite_fig_1.py
@@ -4,6 +4,7 @@ import gzip
 import json
 import os
 import subprocess
+import csv
 from pathlib import Path
 
 import matplotlib.pyplot as plt  # type: ignore
@@ -12,6 +13,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns  # type: ignore
 from matplotlib.gridspec import GridSpec  # type: ignore
+from collections import defaultdict
 
 dashboard = os.path.expanduser("~/code/mgs-pipeline/dashboard/")
 
@@ -31,21 +33,197 @@ with open(os.path.join(dashboard, "metadata_papers.json")) as inf:
 with open(os.path.join(dashboard, "taxonomic_names.json")) as inf:
     taxonomic_names = json.load(inf)
 
+if not os.path.exists("../taxonomy"):
+    os.mkdir("../taxonomy")
 
-studies = list(metadata_papers.keys())
+
+TARGET_STUDY_METADATA = {
+    "Bengtsson-Palme": {
+        "bioprojects": ["PRJEB14051"],
+        "year": 2016,
+    },
+    "Munk": {
+        "bioprojects": ["PRJEB13831", "PRJEB27054", "PRJEB27621", "PRJEB40798", "PRJEB40815", "PRJEB40816", "PRJEB51229"],
+        "year": 2022,
+    },
+    "Brinch": {
+        "bioprojects": ["PRJEB13832", "PRJEB34633"],
+        "year": 2020,
+    },
+    "Ng": {
+        "bioprojects": ["PRJNA438174"],
+        "year": 2019,
+    },
+    "Maritz": {
+        "bioprojects": ["PRJEB28033"],
+        "year": 2019,
+    },
+    "Brumfield": {
+        "bioprojects": ["PRJNA812772"],
+        "year": 2022,
+    },
+    "Yang": {
+        "bioprojects": ["PRJNA645711"],
+        "year": 2020,
+    },
+    "Spurbeck": {
+        "bioprojects": ["PRJNA924011"],
+        "year": 2023,
+    },
+    "CC": {
+        "bioprojects": ["PRJNA661613"],
+        "year": 2021,
+    },
+    #"Rothman": { # not yet run through the pipeline
+    #    "bioprojects": ["PRJNA729801"],
+    #    "year": 2021,
+    #},
+}
+
+
+taxonomic_files = ["taxonomic_composition", "hv_clade_counts", "kraken_reports"]
+metadata_file = "sample-metadata"
+
+
+for study, metadata_study in TARGET_STUDY_METADATA.items():
+    for bioproject in metadata_study["bioprojects"]:
+        for fname in taxonomic_files:
+            if not os.path.exists(f"../taxonomy/{study}-{bioproject}/{fname}.tsv"):
+                subprocess.run(['aws', 's3', 'cp', f's3://nao-mgs-wb/{study}-{bioproject}/output/{fname}.tsv.gz', f"../taxonomy/{study}-{bioproject}/{fname}.tsv.gz"])
+                subprocess.run(['gzip', '-d', f"../taxonomy/{study}-{bioproject}/{fname}.tsv.gz"])
+        if not os.path.exists(f"../taxonomy/{study}-{bioproject}/{metadata_file}.csv"):
+            subprocess.run(['aws', 's3', 'cp', f's3://nao-mgs-wb/{study}-{bioproject}/output/{metadata_file}.csv', f"../taxonomy/{study}-{bioproject}/{metadata_file}.csv"])
+
+
+
+current_directory = os.getcwd()
+if not current_directory.endswith("figures"):
+    raise Exception(
+        "Current directory is not 'figures'. Please change to the 'figures' directory to run this script."
+    )
 
 
 target_taxa = {
     10239: ("viruses", "Viruses"),
-    2731341: ("duplodnaviria", "DNA Viruses"),
-    2732004: ("varidnaviria", "DNA Viruses"),
-    2731342: ("monodnaviria", "DNA Viruses"),
-    2842242: ("ribozyviria", "RNA Viruses"),
-    687329: ("anelloviridae", "DNA Viruses"),
-    2559587: ("riboviria", "RNA Viruses"),
-    2840022: ("adnaviria", "DNA Viruses"),
+    2731341: ("Duplodnaviria", "DNA Viruses"),
+    2732004: ("Varidnaviria", "DNA Viruses"),
+    2731342: ("Monodnaviria", "DNA Viruses"),
+    2559587: ("Riboviria", "RNA Viruses"),
+    #2842242: ("ribozyviria", "RNA"),  # not in hv_clade_counts
+    #687329: ("anelloviridae", "DNA"), # not in hv_clade_counts
+    #2840022: ("adnaviria", "DNA"), # not in hv_clade_counts
     9999999999: ("human viruses", "Viruses"),
 }
+
+
+
+
+def assemble_plotting_dfs() -> tuple[pd.DataFrame, pd.DataFrame]:
+    viral_composition_data = []
+    hv_genus_data = []
+    for study, metadata_study in TARGET_STUDY_METADATA.items():
+        for bioproject in metadata_study["bioprojects"]:
+            study_bioproject = f"{study}-{bioproject}"
+
+            metadata_samples = {}
+            with open(f"../taxonomy/{study_bioproject}/sample-metadata.csv", mode='r', encoding='utf-8-sig') as file:
+                reader = csv.DictReader(file)
+                for row in reader:
+                    sample = row.pop('sample')
+                    metadata_samples[sample] = row
+
+
+            fine_taxonomy = pd.read_csv(f"../taxonomy/{study_bioproject}/kraken_reports.tsv", sep="\t")
+            fine_taxonomy_dict = {sample: df for sample, df in fine_taxonomy.groupby("sample")}
+
+
+            high_level_taxonomy = pd.read_csv(f"../taxonomy/{study_bioproject}/taxonomic_composition.tsv", sep="\t")
+
+            hv_clade_counts = pd.read_csv(f"../taxonomy/{study_bioproject}/hv_clade_counts.tsv", sep="\t")
+
+
+
+            samples = metadata_samples.keys()
+
+            if study == "Bengtsson-Palme":
+                samples = [
+                    sample
+                    for sample in samples
+                    if metadata_samples[sample]["sample_type"].startswith(
+                        "Inlet"
+                    )
+                ]
+
+            if study == "Ng":
+                samples = [
+                    sample
+                    for sample in samples
+                    if metadata_samples[sample]["sample_type"] == "Influent"
+                ]
+
+
+            hv_virus_species = hv_clade_counts[hv_clade_counts["rank"] == "species"]["rank"].unique()
+
+            for sample in samples:
+
+                if metadata_samples[sample].get("enrichment") == "panel":
+                    continue
+                sample_hv_clade_counts = hv_clade_counts[hv_clade_counts["sample"] == sample]
+                hv_clade_counts_dict = dict(zip(sample_hv_clade_counts['taxid'], sample_hv_clade_counts['n_reads_clade']))
+                hv_genus_data.append({
+                    "study": study,
+                    "sample": sample,
+                    **hv_clade_counts_dict
+                })
+
+
+                total_filtered_reads = high_level_taxonomy[(high_level_taxonomy["sample"] == sample) & (high_level_taxonomy["classification"] == "Filtered")]["n_reads"].iloc[0]
+
+                sample_fine_taxonomy = fine_taxonomy_dict[sample]
+
+                tax_relative_abundance_dict = sample_fine_taxonomy.set_index("taxid")["pc_reads_total"].to_dict()
+
+                hv_reads = hv_clade_counts_dict.get(10239, 0) # if no human infecting viruses, return 0
+                hv_relative_abundance = hv_reads / total_filtered_reads
+
+
+                taxa_abundances = {
+                    "DNA Viruses": 0,
+                    "RNA Viruses": 0,
+                    "Viruses": 0,
+                }
+
+                for taxid in target_taxa.keys():
+                    nucleic_acid_type = target_taxa[taxid][1]
+                    tax_relative_abundance_perc = tax_relative_abundance_dict.get(taxid, 0)
+                    tax_relative_abundance = tax_relative_abundance_perc / 100 # % to fraction
+
+                    taxa_abundances[nucleic_acid_type] += tax_relative_abundance
+
+                viral_composition_data.append(
+                    {
+                        "study": study,
+                        "sample": sample,
+                        "Human-Infecting Viruses": hv_relative_abundance,
+                        **taxa_abundances,
+                    }
+                )
+
+    viral_composition_df = pd.DataFrame(viral_composition_data)
+    viral_composition_df = shape_vir_comp_df(viral_composition_df)
+
+    hv_genus_df = pd.DataFrame(hv_genus_data)
+    hv_genus_df = shape_hv_genus_df(hv_genus_df)
+
+    study_nucleic_acid_mapping = get_study_nucleic_acid_mapping()
+
+    viral_composition_df = order_df(viral_composition_df, study_nucleic_acid_mapping)
+    hv_genus_df = order_df(hv_genus_df, study_nucleic_acid_mapping)
+
+
+    return viral_composition_df, hv_genus_df
+
+
 
 
 def order_df(
@@ -66,9 +244,9 @@ def order_df(
     return df
 
 
-def shape_boxplot_df(boxplot_df: pd.DataFrame) -> pd.DataFrame:
-    boxplot_df = boxplot_df[
-        (boxplot_df.drop(["study", "sample"], axis=1) != 0).all(1)
+def shape_vir_comp_df(viral_composition_df: pd.DataFrame) -> pd.DataFrame:
+    viral_composition_df = viral_composition_df[
+        (viral_composition_df.drop(["study", "sample"], axis=1) != 0).all(1)
     ].melt(
         id_vars=["study", "sample"],
         value_vars=[
@@ -81,31 +259,31 @@ def shape_boxplot_df(boxplot_df: pd.DataFrame) -> pd.DataFrame:
         value_name="Relative Abundance",
     )
 
-    boxplot_df["Relative Abundance"] = boxplot_df["Relative Abundance"].apply(
+    viral_composition_df["Relative Abundance"] = viral_composition_df["Relative Abundance"].apply(
         np.log10
     )
 
-    return boxplot_df
+    return viral_composition_df
 
 
-def shape_barplot_df(barplot_df: pd.DataFrame) -> pd.DataFrame:
+def shape_hv_genus_df(hv_genus_df: pd.DataFrame) -> pd.DataFrame:
     taxid_parents = load_taxonomic_data()
 
     unique_numeric_cols = set(
-        col for col in barplot_df.columns if isinstance(col, int)
+        col for col in hv_genus_df.columns if isinstance(col, int)
     )
 
     species_family_mapping = {
         col: get_family(col, taxid_parents) for col in unique_numeric_cols
     }
 
-    barplot_df.rename(columns=species_family_mapping, inplace=True)
+    hv_genus_df.rename(columns=species_family_mapping, inplace=True)
 
-    melted_df = barplot_df.melt(
+    melted_df = hv_genus_df.melt(
         id_vars=["study", "sample"],
         value_vars=[
             col
-            for col in barplot_df.columns
+            for col in hv_genus_df.columns
             if col not in ["study", "sample"] and not pd.isna(col)
         ],
         var_name="family_taxid",
@@ -202,163 +380,12 @@ def get_taxid_name(
     return tax_name
 
 
-def assemble_plotting_dfs() -> tuple[pd.DataFrame, pd.DataFrame]:
-    box_plot_data = []
-    bar_plot_data = []
-    for study in studies:
-        # Dropping studies that aren't WTP based
-        if study not in [
-            "Bengtsson-Palme 2016",
-            "Munk 2022",
-            "Brinch 2020",
-            "Ng 2019",
-            "Maritz 2019",
-            "Brumfield 2022",
-            "Rothman 2021",
-            "Yang 2020",
-            "Spurbeck 2023",
-            "Crits-Christoph 2021",
-        ]:
-            continue
-
-        for bioproject in metadata_papers[study]["projects"]:
-            samples = metadata_bioprojects[bioproject]
-
-            if study == "Bengtsson-Palme 2016":
-                samples = [
-                    sample
-                    for sample in samples
-                    if metadata_samples[sample]["fine_location"].startswith(
-                        "Inlet"
-                    )
-                ]
-
-            if study == "Ng 2019":
-                samples = [
-                    sample
-                    for sample in samples
-                    if metadata_samples[sample]["fine_location"] == "Influent"
-                ]
-
-            for sample in samples:
-                modified_study = study
-                if metadata_samples[sample].get("enrichment") == "panel":
-                    continue
-                if study == "Brumfield 2022":
-                    if metadata_samples[sample]["na_type"] == "DNA":
-                        modified_study = "Brumfield 2022\n(DNA Subset)"
-
-                    else:
-                        modified_study = "Brumfield 2022\n(RNA Subset)"
-
-                humanreads = "%s.humanviruses.tsv" % sample
-
-                if not os.path.exists(f"../humanviruses/{humanreads}"):
-                    subprocess.check_call(
-                        [
-                            "aws",
-                            "s3",
-                            "cp",
-                            "s3://nao-mgs/%s/humanviruses/%s"
-                            % (bioproject, humanreads),
-                            "humanviruses/",
-                        ]
-                    )
-
-                with open(f"../humanviruses/{humanreads}") as inf:
-                    human_virus_counts = {}
-                    human_virus_reads = 0
-                    for line in inf:
-                        (
-                            line_taxid,
-                            clade_assignments,
-                            _,
-                        ) = line.strip().split("\t")
-                        clade_assignments = int(clade_assignments)
-                        line_taxid = int(line_taxid)
-
-                        human_virus_counts[line_taxid] = clade_assignments 
-                        human_virus_reads += int(clade_assignments)
-
-                    human_virus_relative_abundance = (
-                        human_virus_reads / metadata_samples[sample]["reads"]
-                    )
-
-                    bar_plot_data.append(
-                        {
-                            "study": modified_study,
-                            "sample": sample,
-                            **human_virus_counts,
-                        }
-                    )
-
-                cladecounts = "%s.tsv.gz" % sample
-                if not os.path.exists(f"../cladecounts/{cladecounts}"):
-                    subprocess.check_call(
-                        [
-                            "aws",
-                            "s3",
-                            "cp",
-                            "s3://nao-mgs/%s/cladecounts/%s"
-                            % (bioproject, cladecounts),
-                            "cladecounts/",
-                        ]
-                    )
-                with gzip.open(f"../cladecounts/{cladecounts}") as inf:
-                    taxa_abundances = {
-                        "DNA Viruses": 0,
-                        "RNA Viruses": 0,
-                        "Viruses": 0,
-                    }
-                    for line in inf:
-                        (
-                            line_taxid,
-                            _,
-                            _,
-                            clade_assignments,
-                            _,
-                        ) = line.strip().split()
-                        taxid = int(line_taxid)
-                        clade_assignments = int(clade_assignments)
-                        if taxid in target_taxa:
-                            nucleic_acid_type = target_taxa[taxid][1]
-                            relative_abundance = (
-                                clade_assignments / metadata_samples[sample]["reads"]
-                            )
-
-                            taxa_abundances[
-                                nucleic_acid_type
-                            ] += relative_abundance
-
-                    box_plot_data.append(
-                        {
-                            "study": modified_study,
-                            "sample": sample,
-                            "Human-Infecting Viruses": human_virus_relative_abundance,
-                            **taxa_abundances,
-                        }
-                    )
-
-    boxplot_df = pd.DataFrame(box_plot_data)
-    boxplot_df = shape_boxplot_df(boxplot_df)
-
-    barplot_df = pd.DataFrame(bar_plot_data)
-    barplot_df = shape_barplot_df(barplot_df)
-
-    study_nucleic_acid_mapping = get_study_nucleic_acid_mapping()
-
-    boxplot_df = order_df(boxplot_df, study_nucleic_acid_mapping)
-    barplot_df = order_df(barplot_df, study_nucleic_acid_mapping)
-
-    return boxplot_df, barplot_df
-
-
 def return_study_order(boxplot_df: pd.DataFrame) -> list[str]:
     study_nucleic_acid_mapping = get_study_nucleic_acid_mapping()
-    df["na_type"] = df["study"].map(study_nucleic_acid_mapping)
+    boxplot_df["na_type"] = boxplot_df["study"].map(study_nucleic_acid_mapping)
     order = (
-        df[df["na_type"] == "DNA"]["study"].unique()
-        + df[df["na_type"] == "RNA"]["study"].unique()
+        boxplot_df[boxplot_df["na_type"] == "DNA"]["study"].unique()
+        + boxplot_df[boxplot_df["na_type"] == "RNA"]["study"].unique()
     )
 
 
@@ -366,6 +393,8 @@ def boxplot(
     ax: plt.Axes,
     boxplot_df: pd.DataFrame,
 ) -> plt.Axes:
+
+    print(boxplot_df)
     order = [
         "Bengtsson-\nPalme 2016",
         "Munk 2022",
@@ -385,11 +414,10 @@ def boxplot(
         y="study",
         x="Relative Abundance",
         hue="Identifier",
-        # hue_order=order,
+        hue_order=order,
         width=0.7,
         showfliers=False,
         ax=ax,
-        order=order,
     )
 
     ax_title = ax.set_title("a", fontweight="bold")
@@ -515,22 +543,26 @@ def start():
     fig = plt.figure(
         figsize=(9, 11),
     )
-
+#
     gs = GridSpec(2, 2, height_ratios=[9, 7], figure=fig)
-
+#
     boxplot_ax = boxplot(
         fig.add_subplot(gs[0, :]),
         boxplot_df,
     )
-
+#
     study_order = [text.get_text() for text in boxplot_ax.get_yticklabels()]
-
+#
     barplot(fig.add_subplot(gs[1, :]), barplot_df, study_order)
-
+#
     plt.tight_layout()
-    save_plot(fig, figdir, "composite_fig_1")
+    print("Watch out, this doesn't include Rothman atm.")
+    print("Watch out, Bengtsson-Palme has no information on RNA vs DNA.")
+    print("You still need to fix the unknown virus issue in hv_clade_counts.")
+    print("is relative abundance based on filtered reads as denominator?")
+
+    #save_plot(fig, figdir, "composite_fig_1")
 
 
 if __name__ == "__main__":
     start()
-
