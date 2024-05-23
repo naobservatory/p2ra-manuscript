a9fa925d1c52f7452ab781037d10afdba6b863a8
simonleandergrimm <simonleandergrimm@gmail.com>
2024-05-23 14:34:27 -0400

    dropped usage of pipeline v1 files, with the exception of metadata papers file.

--- figures/composite_fig_1.py
+++ figures/composite_fig_1.py
@@ -18,20 +18,9 @@ from collections import defaultdict
 dashboard = os.path.expanduser("~/code/mgs-pipeline/dashboard/")
 
 
-with open(os.path.join(dashboard, "human_virus_sample_counts.json")) as inf:
-    human_virus_sample_counts = json.load(inf)
-
-with open(os.path.join(dashboard, "metadata_samples.json")) as inf:
-    metadata_samples = json.load(inf)
-
-with open(os.path.join(dashboard, "metadata_bioprojects.json")) as inf:
-    metadata_bioprojects = json.load(inf)
-
 with open(os.path.join(dashboard, "metadata_papers.json")) as inf:
     metadata_papers = json.load(inf)
 
-with open(os.path.join(dashboard, "taxonomic_names.json")) as inf:
-    taxonomic_names = json.load(inf)
 
 if not os.path.exists("../taxonomy"):
     os.mkdir("../taxonomy")
@@ -583,6 +572,7 @@ def start():
     print(
         "fix thing where the composition dfs are being saved and reused as CSVs"
     )
+    print("stop usage of metadata papers file")
 
     save_plot(fig, figdir, "composite_fig_1")
 
